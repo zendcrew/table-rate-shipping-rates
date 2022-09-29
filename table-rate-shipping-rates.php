c65d343da3436ccda0bb464065e8a9838d702c5e
zendcrew <info@zendcrew.com>
2022-09-29 02:59:48 +0100

    Updated framework

--- table-rate-shipping-rates.php
+++ table-rate-shipping-rates.php
@@ -4,7 +4,7 @@
  * Plugin Name: Shipped - WooCommerce Table Rate Shipping
  * Plugin URI: https://wordpress.org/plugins/table-rate-shipping-rates
  * Description: Create multiple shipping rates & handling fees based on product rules and cart conditions
- * Version: 1.0
+ * Version: 1.0.1
  * Author: zendcrew
  * Author URI: https://codecanyon.net/user/zendcrew/portfolio?ref=zendcrew
  * Text Domain: table-rate-shipping-rates
